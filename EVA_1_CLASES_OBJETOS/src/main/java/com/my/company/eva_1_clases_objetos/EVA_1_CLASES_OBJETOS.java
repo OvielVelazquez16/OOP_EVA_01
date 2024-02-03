b584e7f9f06c15e42ac9d108162b91b7650343d2
OvielVelazquez16 <158379953+OvielVelazquez16@users.noreply.github.com>
2024-02-02 18:55:16 -0600

    Add files via upload

---
+++ EVA_1_CLASES_OBJETOS/src/main/java/com/my/company/eva_1_clases_objetos/EVA_1_CLASES_OBJETOS.java
@@ -0,0 +1,25 @@
+
+
+package com.my.company.eva_1_clases_objetos;
+
+/**
+ *
+ * @author Oviel
+ */
+public class EVA_1_CLASES_OBJETOS {
+
+    public static void main(String[] args) {
+        Persona persol; //declaro un identificaror (variable) de tipo Persona
+        //solo he declarado la variable, no he creado el objeto
+        //crear un objeto (instanciar) consiste en asignar memoria
+        int valor;
+    }
+}
+
+//CLASE PERSONA
+/*class NombreDeLaClase 
+{ cuerpo de la clase}*/ 
+
+class Persona{
+}
+
