358fc71e6562b80727ee7e40e520d30587cb4b9b
OvielVelazquez16 <158379953+OvielVelazquez16@users.noreply.github.com>
2024-02-08 19:03:03 -0600

    Add files via upload

---
+++ PRACTICA_4_MODIFICADORES_ACCESO/src/main/java/paquete_1/a.java
@@ -0,0 +1,32 @@
+
+package paquete_1;
+
+import paquete_2.b;
+import paquete_2.c;
+
+/**
+ *
+ * @author Oviel
+ */
+public class a {
+   
+
+    public static void main(String[] args) {
+       one obj1 = new one ();
+        System.out.println(obj1);
+        
+        //clases del paquete 2
+        b objb = new b ();
+        System.out.println(objb);
+       
+        //Two obj2 =new two
+        
+     // Three obj3 = new Three();
+     c objc = new c ();
+        System.out.println(objc);
+        
+    }
+}
+class one{
+    
+}
